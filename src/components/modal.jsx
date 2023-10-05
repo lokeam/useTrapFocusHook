b58f4bf4dd9dfd547411f5c974d17fcde7bd1bf4
Ahn Ming Loke <lokeahnming@gmail.com>
2023-10-05 17:32:03 -0400

    style: center modal in viewport, add overlay

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDom from 'react-dom'
 import { PropTypes } from 'prop-types';
+import './modal.css';
 
 const Modal = ({ isOpen, children, onClose }) => {
   if (!isOpen) return null;
 
   return ReactDom.createPortal(
     <React.Fragment>
-      <div className="modal_wrapper">
-        <h1>Modal</h1>
-        <button className="modal_close_button" onClick={onClose}>close modal</button>
-        {children}
+      <div className="modal__overlay">
+        <div className="modal__wrapper">
+          <h1>Modal</h1>
+          <button className="modal_close_button" onClick={onClose}>close modal</button>
+          {children}
+        </div>
       </div>
     </React.Fragment>,
     document.getElementById('root')
